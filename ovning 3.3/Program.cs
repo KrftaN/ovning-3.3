12b78816a30a5413ee523595c78660d0e71dce16
KraftaN <63770425+KrftaN@users.noreply.github.com>
2024-05-31 13:46:30 +0200

    Add project files.

---
+++ ovning 3.3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace övning_3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int hourlyRate = 80;
+            const int maxDailyRate = 950;
+
+            Console.WriteLine("Hur många timmar vill du hyra bilen?");
+            int hours = int.Parse(Console.ReadLine());
+
+            int cost = hours * hourlyRate;
+
+            if (cost > maxDailyRate)
+            {
+                cost = maxDailyRate;
+            }
+
+            Console.WriteLine($"Det kommer att kosta {cost} kronor att hyra bilen i {hours} timmar.");
+        }
+    }
+}
